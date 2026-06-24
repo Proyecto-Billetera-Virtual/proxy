97db5aa83d44500998a8609eb9f0cac302fd7f91
blevatino <b.levatino@alumno.etec.um.edu.ar>
2026-06-24 11:44:26 -0300

    correciones finales

--- src/index.ts
+++ src/index.ts
@@ -16,15 +16,68 @@ const URL_PASARELA = process.env.URL_PASARELA || 'http://localhost:3001';
 
 // 3. MIDDLEWARE (CAPA INTERMEDIA)
 app.use(express.json());
-app.use('/api', proxy(URL_BACKEND));
-app.use('/pasarela', proxy(URL_PASARELA));
-app.use('/api/interno', proxy(URL_BACKEND)); // canal interno para backend y pasarela
 
-// Middleware para manejar logs
-app.use((req: Request, res: Response, next: any) => {
-    console.log(`[${new Date().toLocaleTimeString()}] Petición recibida: ${req.method} ${req.url}`);
-    next(); // Crucial para que la petición no se quede colgada
-});
+
+
+// Proxy para el Backend Estándar
+app.use('/api', proxy(URL_BACKEND, {
+    // 1. IDA: Cuando el proxy le reenvía al Backend
+    proxyReqOptDecorator: (proxyReqOpts: any, srcReq: any) => {
+        console.log(`Frontend -> Proxy: ${srcReq.method} ${srcReq.url}`);
+        console.log(`Reenviando a Destinatario (Backend) -> ${URL_BACKEND}${proxyReqOpts.path}\n`);
+        return proxyReqOpts;
+    },
+    // 2. VUELTA: Cuando el Backend responde con éxito
+    userResDecorator: (proxyRes: any, proxyResData: any, userReq: any, userRes: any) => {
+        console.log(`Confirmación desde Backend: Estado ${proxyRes.statusCode}`);
+        console.log(`Enviando respuesta de regreso al Frontend\t `);
+        return proxyResData; // Envía los datos intactos al front
+    }
+}));
+
+// Proxy para la Pasarela de Pagos
+app.use('/pasarela', proxy(URL_PASARELA, {
+    proxyReqOptDecorator: (proxyReqOpts: any, srcReq: any) => {
+        console.log(`Frontend -> Proxy: ${srcReq.method} ${srcReq.url}`);
+        console.log(`Reenviando a Destinatario (Pasarela) -> ${URL_PASARELA}${proxyReqOpts.path}\n`);
+        return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes: any, proxyResData: any, userReq: any, userRes: any) => {
+        console.log(`Confirmación desde Pasarela: Estado ${proxyRes.statusCode}`);
+        console.log(`Enviando respuesta de regreso al Frontend\t `);
+        return proxyResData;
+    }
+}));
+
+// Canal Privado: Pasarela -> Backend
+app.use('/api/interno', proxy(URL_BACKEND, {
+    proxyReqOptDecorator: (proxyReqOpts: any, srcReq: Request) => {
+        console.log(`Pasarela -> Proxy (Canal Privado): ${srcReq.method} ${srcReq.url}`);
+        console.log(`Reenviando a Destinatario (Backend) -> ${URL_BACKEND}${proxyReqOpts.path}\n`);
+        return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes: any, proxyResData: any, userReq: Request, userRes: Response) => {
+        console.log(`Confirmación desde Backend: Estado ${proxyRes.statusCode}`);
+        console.log(`Enviando respuesta de regreso a Pasarela\t `);
+        return proxyResData;
+    }
+}));
+
+// ¡NUEVO! Canal Privado: Backend -> Pasarela
+app.use('/pasarela/interno', proxy(URL_PASARELA, {
+    proxyReqOptDecorator: (proxyReqOpts: any, srcReq: Request) => {
+        console.log(`Backend -> Proxy (Canal Privado): ${srcReq.method} ${srcReq.url}`);
+        console.log(`Reenviando a Destinatario (Pasarela) -> ${URL_PASARELA}${proxyReqOpts.path}\n`);
+        return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes: any, proxyResData: any, userReq: Request, userRes: Response) => {
+        console.log(`Confirmación desde Pasarela: Estado ${proxyRes.statusCode}`);
+        console.log(`Enviando respuesta de regreso al Backend\t `);
+        return proxyResData;
+    }
+}));
+
+
 
 // 4. CONFIGURACIÓN DE UNA RUTA (ENDPOINT DE PRUEBA)
 app.get('/health', (req: Request, res: Response) => {
